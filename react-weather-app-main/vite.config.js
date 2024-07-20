cbcd31d957a8e1f73c8f1c6b9e25ca402991c5a7
bharathipriyan08 <fraudbharathi@gmail.com>
2024-07-20 13:29:12 +0530

    Success

--- react-weather-app-main/vite.config.js
+++ react-weather-app-main/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base: '/react-weather-app/'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-weather-app/'
 })
